36af23d33fe19ff364df07e4b760af4ff7da8fc3
fzf003 <fzf003@gmail.com>
2023-09-21 15:32:00 +0800

    Update ProjectManager.cs

--- ProjectManager.cs
+++ ProjectManager.cs
@@ -1,1 +1,52 @@
 
+public interface IProjection
+{
+    Type[] Handles { get; }
+    void Handle(object @event);
+}
+
+public abstract class Projection : IProjection
+{
+    private readonly ConcurrentDictionary<Type, Action<object>> handlers = new();
+
+    public Type[] Handles => handlers.Keys.ToArray();
+
+    protected void Projects<TEvent>(Action<TEvent> action)
+    {
+        handlers.GetOrAdd(typeof(TEvent), @event => action((TEvent)@event));
+    }
+
+    public void Handle(object @event)
+    {
+        handlers[@event.GetType()](@event);
+    }
+}
+
+public record UserCreated(string Name, int TaskId)
+{
+
+}
+
+public class MyProjection : Projection
+{
+    public MyProjection()
+    {
+        Projects<UserCreated>(Apply);
+    }
+
+    void Apply(UserCreated created)
+    {
+        Console.WriteLine(created.Name + "-" + created.TaskId);
+    }
+}
+
+
+public class ViewManager
+{
+    public static void Run()
+    {
+        var MyProjection = Activator.CreateInstance(typeof(MyProjection), true)! as MyProjection;
+
+        MyProjection.Handle(new UserCreated("fzf003", 99839));
+    }
+}
