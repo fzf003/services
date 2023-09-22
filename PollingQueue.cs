a1178810367d0daf1c3eabded5f466fdf5cff879
fzf003 <fzf003@gmail.com>
2023-09-22 15:06:32 +0800

    Update PollingQueue.cs

--- PollingQueue.cs
+++ PollingQueue.cs
@@ -1,1 +1,41 @@
 
+using Microsoft.Data.SqlClient;
+ 
+
+public interface IPollingQueue
+{
+    Task<List<UserInfo>> DequeueAsync(CancellationToken cancellationToken);
+}
+
+public class PollingQueue : IPollingQueue
+{
+    readonly Func<SqlConnection> _sqlConnection;
+
+    readonly string TableName = "HD..users";
+
+    readonly int batchCount=10;
+    int index=1;
+    public PollingQueue(Func<SqlConnection> sqlConnection)
+    {
+        _sqlConnection = sqlConnection;
+    }
+
+    public async Task<List<UserInfo>> DequeueAsync(CancellationToken cancellationToken)
+    {
+        var count=batchCount*index;
+
+        using var sqlConnection = _sqlConnection();
+
+        var result = await SqlQueue.GetNextAsync(sqlConnection, TableName,batchCount:count, cancellationToken: cancellationToken);
+
+        while (result.Count == 0 && !cancellationToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, cancellationToken).ConfigureAwait(false);
+            result = await SqlQueue.GetNextAsync(sqlConnection, TableName,batchCount:count, cancellationToken: cancellationToken);
+        }
+
+        index++;
+
+        return result;
+    }
+}
