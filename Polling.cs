add159ef04a3a7a785863e0b4f4e7d143f07da3b
fzf003 <fzf003@gmail.com>
2023-10-10 17:29:44 +0800

    Update Polling.cs

--- Polling.cs
+++ Polling.cs
@@ -11,3 +11,30 @@ public static partial class Polling
 
     internal static IObservable<T> Poll<T>(this IObservable<T> query) => query.Poll(DefaultPoller);
 }
+
+/*
+   
+var Dispatcher = new OutboxDispatcher(() => (SqlConnection)factory.CreateConnection("HD"));
+
+var source = Observable.FromAsync(async () => await PollingQueue.DequeueAsync(CancellationToken.None));
+
+Polling.Poll(source)
+       .SelectMany(p => p)
+       .Select(MessageAckAsync).Retry(2)
+       .Switch()
+       .OnErrorResumeNext(Observable.Return(UserInfo.CreateUser("", "S", DateTime.Now)))
+       .Subscribe(Console.WriteLine, err => Console.WriteLine(err));
+
+
+
+static IObservable<UserInfo> MessageAckAsync(UserInfo user)
+{
+    return Observable.FromAsync(async () =>
+      {
+          using var sqlConnection = new SqlConnection(connectionString);
+          await sqlConnection.OpenAsync().ConfigureAwait(false);
+          await SqlQueue.AckAsync(sqlConnection, user.UserId).ConfigureAwait(false);
+          return user;
+      });
+}
+*/
