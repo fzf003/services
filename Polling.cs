6f36a07155bd5a2fbd061ef282efe4caad46bf14
fzf003 <fzf003@gmail.com>
2023-10-10 17:28:49 +0800

    Update Polling.cs

--- Polling.cs
+++ Polling.cs
@@ -1,1 +1,13 @@
 
+public static partial class Polling
+{
+    public static IObservable<Unit> DefaultPoller = Observable
+        .Timer(TimeSpan.FromMilliseconds(100))
+        .Select(_ => Unit.Default);
+
+    internal static IObservable<T> Poll<T>(this IObservable<T> query, IObservable<Unit> poller) => poller
+        .SelectMany(_ => query)
+        .Repeat();
+
+    internal static IObservable<T> Poll<T>(this IObservable<T> query) => query.Poll(DefaultPoller);
+}
