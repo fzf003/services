d2d59163cfb99f506a4200ddfefe3196ca280258
fzf003 <fzf003@gmail.com>
2023-10-20 16:39:58 +0800

    Update Consumer.cs

--- Consumer.cs
+++ Consumer.cs
@@ -1,1 +1,37 @@
 
+public record Consumer(int Id, string Name, int Age, AddressInfo Address)
+{
+    public Consumer WithConsumerName(string Name)
+    {
+        return this with { Name = Name };
+    }
+
+    public Consumer WithChangeAddress(AddressInfo addressInfo)
+    {
+        if (addressInfo is null)
+        {
+            throw new ArgumentNullException(nameof(addressInfo));
+        }
+
+        return this with { Address = addressInfo };
+    }
+
+    public static Consumer Create(int Id, string Name, int Age, AddressInfo addressInfo)
+    {
+        if (addressInfo is null)
+        {
+            throw new ArgumentNullException(nameof(addressInfo));
+        }
+
+        return new Consumer(Id, Name, Age, addressInfo);
+    }
+}
+
+
+public record AddressInfo(string Address, string Tel, string No)
+{
+    public static AddressInfo Create(string Address, string Tel, string No)
+    {
+         return new AddressInfo(Address, Tel, No);
+    }
+}
