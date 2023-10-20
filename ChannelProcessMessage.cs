2393cd15eef733795ac740855c4b75242bcd4cb0
fzf003 <fzf003@gmail.com>
2023-10-20 16:34:13 +0800

    Update ChannelProcessMessage.cs

--- ChannelProcessMessage.cs
+++ ChannelProcessMessage.cs
@@ -1,1 +1,29 @@
 
+Channel<UserInfo> MessageChannel = Channel.CreateUnbounded<UserInfo>(
+   new UnboundedChannelOptions
+  {
+      SingleWriter = true,
+      SingleReader = false,
+      AllowSynchronousContinuations = true,
+  }
+);
+
+/*
+.CreateBounded<User>(new BoundedChannelOptions(capacity: 1_000)
+{
+    FullMode = BoundedChannelFullMode.Wait,
+    SingleWriter = true,
+    SingleReader = false,
+    AllowSynchronousContinuations = false,
+});*/
+
+
+_ = Task.Run(async () =>
+{
+    MessageChannel.Reader.ToObservable().Subscribe(item => Console.WriteLine("One-" + item));
+});
+
+_ = Task.Run(async () =>
+{
+    MessageChannel.Reader.ToObservable().Subscribe(item => Console.WriteLine("two-" + item));
+});
