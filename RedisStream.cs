6eafefbae3fdfca91d06c4eedc7f6762ebab6900
fzf003 <fzf003@gmail.com>
2023-11-10 14:49:56 +0800

    Update RedisStream.cs

--- RedisStream.cs
+++ RedisStream.cs
@@ -1,5 +1,7 @@
-
  
+using System.Formats.Tar;
+using StackExchange.Redis;
+
 CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
 var CancellationToken = cancellationTokenSource.Token;
 
@@ -11,6 +13,7 @@ var ConsumerName = "consumer_2";
 
 var database = connectionMultiplexer.GetDatabase(5);
 
+//await database.KeyDeleteAsync(streamName).ConfigureAwait(false);
 
 if (!await database.KeyExistsAsync(streamName))
 {
@@ -20,17 +23,56 @@ if (!await database.KeyExistsAsync(streamName))
 
 
 static Dictionary<string, string> ParseMessage(StreamEntry entry) => entry.Values.ToDictionary(x => x.Name.ToString(), x => x.Value.ToString());
- 
-/*
+
+
 var StreamReader = Task.Run(async () =>
+{
+    
+    while (!CancellationToken.IsCancellationRequested)
+    {
+        var messages = await database.StreamRangeAsync(streamName, minId: "-", maxId: "+");
+
+        if (messages.Any())
+        {
+          // Console.WriteLine( messages.Select(p=>p.Id).Distinct().Count());
+
+            foreach (var item in messages)
+            {
+                var dict = ParseMessage(item);
+
+                foreach (var mitem in dict)
+                {
+                    Console.WriteLine(item.Id + "--" + mitem.Key + "---" + mitem.Value);
+                }
+            }
+  
+        }
+
+        await Task.Delay(1000).ConfigureAwait(false);
+    }
+
+});
+
+var ConsumerInfo = Task.Run(async () =>
 {
     while (!CancellationToken.IsCancellationRequested)
     {
-        var messages = await database.StreamRangeAsync(streamName,
-        minId: "0-0",
-        maxId: "+",
-        count: 10,
-        messageOrder: Order.Descending);
+        var pendingInfo = database.StreamPending(streamName, streamGroup);
+        Console.WriteLine(pendingInfo.PendingMessageCount);
+        Console.WriteLine(pendingInfo.LowestPendingMessageId);
+        Console.WriteLine(pendingInfo.HighestPendingMessageId);
+        Console.WriteLine($"Consumer count: {pendingInfo.Consumers.Length}.");
+        Console.WriteLine(pendingInfo.Consumers.First().Name);
+        Console.WriteLine(pendingInfo.Consumers.First().PendingMessageCount);
+        await Task.Delay(1000).ConfigureAwait(false);
+    }
+});
+
+var ConsumerGroup = Task.Run(async () =>
+{
+    while (!CancellationToken.IsCancellationRequested)
+    {
+        var messages = database.StreamReadGroup(streamName, streamGroup, ConsumerName, ">", count: 1, noAck: false);
 
         if (messages.Any())
         {
@@ -40,65 +82,41 @@ var StreamReader = Task.Run(async () =>
 
             foreach (var item in dict)
             {
-                Console.WriteLine(message.Id + "--" + item.Key + "---" + item.Value);
+                Console.WriteLine("消费者:" + ConsumerName + "--" + message.Id + "--" + item.Key + "---" + item.Value);
             }
 
+            await database.StreamAcknowledgeAsync(streamName, streamGroup, message.Id).ConfigureAwait(false);
         }
 
-        await Task.Delay(1000).ConfigureAwait(false);
-    }
-
-});*/
-
-var ConsumerGroup = Task.Run(async () =>
-{
-    while (!CancellationToken.IsCancellationRequested)
-    {
-       var messages= database.StreamReadGroup(streamName, streamGroup, ConsumerName, ">", count: 5,noAck:false);
-
-       if(messages.Any())
-       {
-            var message = messages.First();
-
-            var dict = ParseMessage(message);
-
-            foreach (var item in dict)
-            {
-                Console.WriteLine("消费者:"+ConsumerName+"--"+message.Id + "--" + item.Key + "---" + item.Value);
-            }
-
-          await  database.StreamAcknowledgeAsync(streamName,streamGroup,message.Id).ConfigureAwait(false);
-       }
-
         await Task.Delay(100).ConfigureAwait(false);
-     }
+    }
 
 });
 
 var ConsumerGroup2 = Task.Run(async () =>
 {
-    string currconsumername="consumer_3";
+    string currconsumername = "consumer_3";
     while (!CancellationToken.IsCancellationRequested)
     {
-       var messages= database.StreamReadGroup(streamName, streamGroup, currconsumername, ">", count: 5,noAck:false);
+        var messages = database.StreamReadGroup(streamName, streamGroup, currconsumername, ">", count: 1, noAck: false);
 
-       if(messages.Any())
-       {
+        if (messages.Any())
+        {
             var message = messages.First();
 
             var dict = ParseMessage(message);
 
             foreach (var item in dict)
             {
-                Console.WriteLine("消费者:"+currconsumername+"--"+message.Id + "--" + item.Key + "---" + item.Value);
+                Console.WriteLine("消费者:" + currconsumername + "--" + message.Id + "--" + item.Key + "---" + item.Value);
             }
 
-            await  database.StreamAcknowledgeAsync(streamName,streamGroup,message.Id).ConfigureAwait(false);
-       }
-      
+            await database.StreamAcknowledgeAsync(streamName, streamGroup, message.Id).ConfigureAwait(false);
+        }
+
 
         await Task.Delay(100).ConfigureAwait(false);
-     }
+    }
 
 });
 
@@ -115,7 +133,7 @@ var publisher = Task.Run(async () =>
             new("temp", DateTime.Now.ToString())
         };
 
-         await database.StreamAddAsync(streamName, values).ConfigureAwait(false);
+        //await database.StreamAddAsync(streamName, values).ConfigureAwait(false);
 
         await Task.Delay(500).ConfigureAwait(false);
     }
@@ -125,7 +143,38 @@ var publisher = Task.Run(async () =>
 
 
 
-await Task.WhenAll(publisher, ConsumerGroup,ConsumerGroup2).ConfigureAwait(false);
+await Task.WhenAll(publisher, ConsumerGroup, ConsumerGroup2, StreamReader).ConfigureAwait(false);
 
 Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static async Task<ConnectionMultiplexer> GetConnectionMultiplexer()
+{
+    ConfigurationOptions config = new ConfigurationOptions
+    {
+        EndPoints =
+    {
+        { "127.0.0.1", 6382 }
+     },
+
+        Password = ""
+    };
+    return await ConnectionMultiplexer.ConnectAsync(config);
+}
+
+
+
 
