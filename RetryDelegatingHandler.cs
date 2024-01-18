4b7e41e2131472c7cd8b0960343b3201f314fb36
fzf003 <fzf003@gmail.com>
2024-01-19 00:09:44 +0800

    Create RetryDelegatingHandler.cs

---
+++ RetryDelegatingHandler.cs
@@ -0,0 +1,61 @@
+public class RetryDelegatingHandler : DelegatingHandler
+{
+    private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy =
+        Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .RetryAsync(2);
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var policyResult = await _retryPolicy.ExecuteAndCaptureAsync(
+            () => base.SendAsync(request, cancellationToken));
+
+        if (policyResult.Outcome == OutcomeType.Failure)
+        {
+            throw new HttpRequestException(
+                "Something went wrong",
+                policyResult.FinalException);
+        }
+
+        return policyResult.Result;
+    }
+}
+
+public class LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger)
+    : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            logger.LogInformation("Before HTTP request");
+
+            var result = await base.SendAsync(request, cancellationToken);
+
+            result.EnsureSuccessStatusCode();
+
+            logger.LogInformation("After HTTP request");
+
+            return result;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "HTTP request failed");
+
+            throw;
+        }
+    }
+}
+
+builder.Services.AddTransient<RetryDelegatingHandler>();
+
+builder.Services.AddHttpClient<GitHubService>(httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://api.github.com");
+})
+.AddHttpMessageHandler<LoggingDelegatingHandler>()
+.AddHttpMessageHandler<RetryDelegatingHandler>();
