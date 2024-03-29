b735be16b66381f423eb335b914f5e0396f67394
fzf003 <fzf003@gmail.com>
2024-03-29 17:21:03 +0800

    Update SqlQueueExtensions.cs

--- SqlQueueExtensions.cs
+++ SqlQueueExtensions.cs
@@ -1,37 +1,71 @@
-public static class SqlQueueExtensions
-{
+using System.Data;
+using Microsoft.Data.SqlClient;
 
-    public static void AddParameter(this SqlCommand command, string name, SqlDbType type, object value)
+public static partial class SqlQueueExtensions
+{
+ 
+    public static SqlCommand AddParameter(this SqlCommand command, string name, SqlDbType type, object value)
     {
         command.Parameters.Add(name, type).Value = value ?? DBNull.Value;
+        return command;
     }
 
-    public static void AddParameter(this SqlCommand command, string name, SqlDbType type, object value, int size)
+    public static SqlCommand AddParameter(this SqlCommand command, string name, object value)
+    {
+        command.Parameters.AddWithValue(name, value ?? DBNull.Value);
+        return command;
+    }
+
+    public static SqlCommand AddParameter(this SqlCommand command, string name, SqlDbType type, object value, int size)
     {
         command.Parameters.Add(name, type, size).Value = value ?? DBNull.Value;
+        return command;
     }
 
-
-    public static async Task<string> GetBodyAsync(this SqlDataReader dataReader, int Index, CancellationToken cancellationToken = default)
-    {
-        if (await dataReader.IsDBNullAsync(Index, cancellationToken).ConfigureAwait(false))
-        {
-            return null;
-        }
-        return await dataReader.GetFieldValueAsync<string>(Index, cancellationToken).ConfigureAwait(false);
-    }
-
-
-    public static async Task<string> GetMessageAsync(this SqlDataReader dataReader, int headersIndex, CancellationToken cancellationToken = default)
+    public static async Task<int> GetMessageIdAsync(this SqlDataReader dataReader, int headersIndex, CancellationToken cancellationToken = default)
     {
         if (await dataReader.IsDBNullAsync(headersIndex, cancellationToken).ConfigureAwait(false))
         {
-            return null;
+            return default;
+        }
+        return await dataReader.GetFieldValueAsync<int>(headersIndex, cancellationToken).ConfigureAwait(false);
+    }
+
+
+    public static async Task<string> GetStringAsync(this SqlDataReader dataReader, int headersIndex, CancellationToken cancellationToken = default)
+    {
+        if (await dataReader.IsDBNullAsync(headersIndex, cancellationToken).ConfigureAwait(false))
+        {
+            return default;
+        }
+        return await dataReader.GetFieldValueAsync<string>(headersIndex, cancellationToken).ConfigureAwait(false);
+    }
+
+    public static async Task<DateTime> GetDataTimeAsync(this SqlDataReader dataReader, int headersIndex, CancellationToken cancellationToken = default)
+    {
+        if (await dataReader.IsDBNullAsync(headersIndex, cancellationToken).ConfigureAwait(false))
+        {
+            return default;
         }
 
-        using (var textReader = dataReader.GetTextReader(headersIndex))
+        return await dataReader.GetFieldValueAsync<DateTime>(headersIndex, cancellationToken).ConfigureAwait(false);
+    }
+
+
+    public static async Task<string> GetMessageAsync(this SqlDataReader dataReader, int messageIndex, CancellationToken cancellationToken = default)
+    {
+        if (await dataReader.IsDBNullAsync(messageIndex, cancellationToken).ConfigureAwait(false))
+        {
+            return default;
+        }
+
+        using (var textReader = dataReader.GetTextReader(messageIndex))
         {
             return await textReader.ReadToEndAsync(cancellationToken).ConfigureAwait(false);
         }
     }
 }
+
+
+
+ 
