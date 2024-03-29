36e2d679defd6e8d601fcf5ee39b7d8769ae83e1
fzf003 <fzf003@gmail.com>
2024-03-29 17:26:36 +0800

    Update EventBusExtensions.cs

--- EventBusExtensions.cs
+++ EventBusExtensions.cs
@@ -37,3 +37,72 @@ public static class EventBusExtensions
         return services;
     }
 }
+
+
+
+
+public static class ReadWriteExtensions
+{
+ 
+    public static Task WithCancellation(this Task task, CancellationToken cancellationToken)
+    {
+        if (task.IsCompleted || !cancellationToken.CanBeCanceled)
+            return task;
+
+        var tcs = new TaskCompletionSource<object>();
+        var r = cancellationToken.Register(() => { tcs.SetCanceled(); }, false);
+
+        return Task.WhenAny(task, tcs.Task)
+
+            .ContinueWith(_ => { r.Dispose(); }, TaskContinuationOptions.ExecuteSynchronously)
+
+            .ContinueWith(_ => { }, cancellationToken, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+    }
+    
+ 
+
+    public static IObservable<T> ToObservable<T>(this IAsyncEnumerable<T> asyncEnumerable, CancellationToken cancellation = default)
+    {
+    
+
+        return Observable.Create<T>(async observer =>
+       {
+           while (!cancellation.IsCancellationRequested)
+           {
+               await foreach (var item in asyncEnumerable)
+               {
+                   observer.OnNext(item);
+               }
+               observer.OnCompleted();
+           }
+
+       });
+    }
+ 
+    public static IDisposable ToConsumerSubscribe<T>(this IConsumer<T> consumer,Action<T> action, CancellationToken cancellation = default) where T:IIntegrationEvent
+    {
+         return consumer.ReadMessageStreamAsync().ToObservable(cancellation).Subscribe(action);
+    }
+
+    public static IDisposable ToConsumerSubscribe<T>(this IConsumer<T> consumer,Action<T> action,Action OnComple, CancellationToken cancellation = default) where T:IIntegrationEvent
+    {
+         return consumer.ReadMessageStreamAsync().ToObservable(cancellation).Subscribe(action,OnComple);
+    }
+
+    public static IObservable<T> ToObservable<T>(this ChannelReader<T> channelReader, CancellationToken cancellation = default)
+    {
+        return Observable.Create<T>(async observer =>
+        {
+            while (!cancellation.IsCancellationRequested)
+            {
+                await foreach (var item in channelReader.ReadAllAsync(cancellation))
+                {
+                    observer.OnNext(item);
+                }
+                observer.OnCompleted();
+            }
+
+        });
+    }
+  
+}
