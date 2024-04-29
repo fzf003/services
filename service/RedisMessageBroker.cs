2751859f3cb20e0f518fee4e2286d94ee8e0d8e3
fzf003 <fzf003@gmail.com>
2024-04-29 15:12:00 +0800

    Create RedisMessageBroker.cs

---
+++ service/RedisMessageBroker.cs
@@ -0,0 +1,39 @@
+using StackExchange.Redis;
+
+public class RedisMessageBroker
+{
+    private readonly ConnectionMultiplexer _redis;
+    private readonly ISubscriber _subscriber;
+    public RedisMessageBroker(string connectionString)
+    {
+        _redis = ConnectionMultiplexer.Connect(connectionString);
+        _subscriber = _redis.GetSubscriber();
+    }
+
+    // Publish message
+    public async Task<bool> Publish(string channel, string message)
+    {
+        try
+        {
+            await _subscriber.PublishAsync(RedisChannel.Literal(channel), message);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+     // Subscribe message
+    public async Task<bool> Subscribe(string channel, Action<RedisChannel, RedisValue> handle)
+    {
+        try
+        {
+            await _subscriber.SubscribeAsync(RedisChannel.Literal(channel), handle);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
