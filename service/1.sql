a65e23f2ce1375e2a1596f709ac1cc3575591fcf
fzf003 <fzf003@gmail.com>
2024-05-21 17:06:17 +0800

    Update 1.sql

--- service/1.sql
+++ service/1.sql
@@ -8,6 +8,50 @@
 
 
 
+		  
+DECLARE @CaseSN NVARCHAR(100)='';
+DECLARE @CompanyName NVARCHAR(100)='';
+DECLARE @CompanyCode NVARCHAR(100)='';
+
+DECLARE F1 CURSOR FOR
+--SELECT Fields,Sort  FROM JZDATA..CRM_LineTemplate WHERE ProtocolType in('优选整装服务销售合同') AND  CompanyCode IN(1);
+ SELECT SN as CaseSN,CompanyCode,CompanyName from SCYXDATA..Base_Case_M where CaseProductType in('X-VILLA别墅整装','X-UNIT平墅整装') and State in(1) and CompanyCode not in(732);
+OPEN F1;
+FETCH NEXT FROM F1 INTO @CaseSN,@CompanyName,@CompanyCode
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+PRINT CONCAT(@CaseSN,'-', @CompanyName)
+
+--DECLARE @Case_M_SN NVARCHAR(100)='YXP2404300016';
+ DECLARE @SN NVARCHAR(100)='';
+ IF EXISTS(SELECT A.SN FROM Base_Space_Part_Config_T a WITH(NOLOCK) 
+          INNER JOIN  Base_Space_Part_Config_M b WITH(NOLOCK) ON a.SN=b.SN WHERE b.State=1
+          AND b.Case_M_SN in(@CaseSN) AND SpaceName in('卫生间'))
+  BEGIN
+       SELECT DISTINCT @SN= A.SN
+		FROM Base_Space_Part_Config_T a WITH(NOLOCK) 
+		INNER JOIN  Base_Space_Part_Config_M b WITH(NOLOCK) ON a.SN=b.SN WHERE b.State=1
+		AND b.Case_M_SN in(@CaseSN) and SpaceName in('卫生间')
+
+	    UPDATE Base_Space_Part_Config_T SET SpaceID=73,SpaceName='主卫生间' where ID IN(
+		  SELECT ID FROM Base_Space_Part_Config_T WHERE SN=@SN  and SpaceName in('卫生间')
+		)
+   END
+ 
+
+
+FETCH NEXT FROM F1 INTO @CaseSN,@CompanyName,@CompanyCode
+END
+CLOSE F1;
+DEALLOCATE F1;
+
+
+
+
+
+
+
 
 
  
