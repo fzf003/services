8bac74ea8484d306ce6feed36953f1a09199b5d4
fzf003 <fzf003@gmail.com>
2024-07-19 18:04:05 +0800

    Update 1.js

--- service/1.js
+++ service/1.js
@@ -1,1 +1,44 @@
 
+{
+    "大类":"",
+    "子类":"",
+    "单位":"",
+    "结算信息": [{
+        "分公司": [{
+            "CompanyCode": 2193,
+            "CompanyName": "北京分公司"
+        }, {
+            "CompanyCode": 2320,
+            "CompanyName": "郑州分公司"
+        }],
+       
+        "结算轮次": [{
+                "index": 1,
+                "First": "开工",
+                "Ratio": 10
+            }, {
+                "index": 2,
+                "Second": "隐蔽",
+                "Ratio": 10
+            }, {
+                "index": 3,
+                "Third": "调试",
+                "Ratio": 30
+            },
+            {
+                "index": 5,
+                "Fifth": "交付",
+                "Ratio": 10
+            }]
+    }],
+    "SetUnit": [
+        { "UnitName": "个" },
+        { "UnitName": "副" },
+        { "UnitName": "位" },
+    ],
+    "SetPrices":[
+        {"CompanyId":2193, "CompanyName": "北京分公司","Unit":"900","Villa":"900"},
+        {"CompanyId":2320, "CompanyName": "郑州分公司","Unit":"900","Villa":"900"},
+        {"CompanyId":2194, "CompanyName": "成都分公司","Unit":"900","Villa":"900"}
+    ]
+}
