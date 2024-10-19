2f1449b3757fdc38034663cb75b925ba8d503409
fzf003 <fzf003@gmail.com>
2024-10-19 18:56:31 +0800

    Update 优选物料问题.sql

--- service/优选物料问题.sql
+++ service/优选物料问题.sql
@@ -12,3 +12,13 @@ WHERE   PM_Price_T.M_ID in(17392)
    select * from SCYXDATA..PM_Price_M where id in(17392)
 
  select * from SCYXDATA..PM_Price_T where M_ID in(17392) and ProCode in('PRO20240219_518336_16')
+
+
+ SELECT f.FoolSpacePartName, PM_Price_T.PartId,PM_Price_T.TaskId,PM_Price_T.Id,Num,convertcaigounum,ConvertArea,convertPrice,ProCode,ProName,CaiGouJia,CaiGouJiaTotal,Active_CaiGoujiaTotal,JSPrcieTotal,SalaWay,ISConversion,convertcaigounum,*
+FROM SCYXDATA..PM_Price_T 
+left join SCYXDATA..V_Part_Infor f
+on SCYXDATA..PM_Price_T.PartID=f.ID
+WHERE   PM_Price_T.M_ID in(17392) 
+       And ProCode in('PRO20240219_518336_16') 
+	   --And PM_Price_T.PartId in(4404012)
+	   and SalaWay in('标配')
